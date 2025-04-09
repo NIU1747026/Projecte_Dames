e731576bda6b009aa9513e7add38cda24556ceae
NIU1747026 <lucas.luna@autonoma.cat>
2025-04-09 23:52:20 +0200

    Base arxius .h

--- Source/posicio.h
+++ Source/posicio.h
@@ -6,29 +6,23 @@
 
 using namespace std;
 
-const int N_FILES = 8;
-const int N_COLUMNES = 8;
-
 class Posicio
 {
 public:
-	Posicio() { m_fila = -1; m_columna = -1; }
-	Posicio(int fila, int columna) { m_fila = fila; m_columna = columna; }
-	Posicio(const string& posicio);
+	Posicio() { m_fila = -1; m_columna = -1; };
+	Posicio(int fila, int columna) { m_fila = fila; m_columna = columna; };
+	Posicio(const string& posicio) { m_fila = 7 - posicio[1] + '1'; m_columna = posicio[0] - 'a'; };
 
 	void setPos(int fila, int columna) { m_fila = fila; m_columna = columna; };
-	int getFila() const { return m_fila; }
-	int getColumna() const { return m_columna; }
+	int getFila() const { return m_fila; };
+	int getColumna() const { return m_columna; };
 
-	string toString() const;
+	string toString() const { char x = 7 - m_fila + '1'; char y = m_columna + 'a'; return { y,x }; };
 
-	bool operator==(Posicio& posicio);
+	bool operator==(Posicio& posicio) { return (m_fila == posicio.m_fila && m_columna == posicio.m_columna); };
 
 private:
 	int m_fila, m_columna;
 };
-
-ifstream& operator>>(ifstream& fitxer, Posicio& posicio);
-ofstream& operator<<(ofstream& fitxer, const Posicio& posicio);
 
 #endif
