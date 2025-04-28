2a76d28df43bbfef911513716e3806f6c082b2a7
NIU1747026 <lucas.luna@autonoma.cat>
2025-04-28 10:18:32 +0200

    pipippiipipi

--- Source/fitxes.h
+++ Source/fitxes.h
@@ -34,14 +34,14 @@ public:
 
 	void actualitzaMovimentsFitxa(Fitxa tauler[N_FILES][N_COLUMNES], const Posicio& origen); // actualitza/refa llista de moviments valids de fitxa, tambe posa a les posicions valides;
 
-	void afegeixPosicionsValides(Posicio Valides[MAX_POSICIONS], int& nValides); // afegeix a l'array valides, les posicions valides de la fitxa, amb compte de no repetir-ne cap que ja estigui
+	void afegeixPosicionsValides(Posicio Valides[MAX_POSICIONS], int& nValides) const; // afegeix a l'array valides, les posicions valides de la fitxa, amb compte de no repetir-ne cap que ja estigui
 
-	bool movimentEsValid(const Posicio& desti, Moviment& mov); //mira destins de la llista de moviments y retorna true si hi es i el posa a mov.
-	bool esMillorMoviment(Moviment& mov); //retorna true si no hi ha cap millor moviment
+	const bool movimentEsValid(const Posicio& desti, Moviment& mov) const; //mira destins de la llista de moviments y retorna true si hi es i el posa a mov.
+	const bool esMillorMoviment(const Moviment& mov) const; //retorna true si no hi ha cap millor moviment
 private:
-	bool trobaMoviment(const Posicio& desti, Moviment& mov);
-
-	void getPosicionsValides(Fitxa tauler[N_FILES][N_COLUMNES], const Posicio& origen, bool potmenjar, Posicio posicionsPendents[MAX_POSICIONS], int& nPendents, bool arrpotmenjar[MAX_POSICIONS]);
+	//const bool trobaMoviment(const Posicio& desti, Moviment& mov) const;
+	void insertaPos(const Posicio& pos); // inserta la pos a posicions valides, sempre que no estigui ja posada
+	void getPosicionsValides(Fitxa tauler[N_FILES][N_COLUMNES], const Posicio& origen, bool potmenjar, Posicio posicionsPossibles[MAX_POSICIONS], int& nPossibles, bool arrpotmenjar[MAX_POSICIONS]); //Donats una posicio d'origen i si pot menjar, retorna a l'array les posicions a les que pot anar junt amb un array on a la posició corresponent a la de la posicio possible diu si pot seguir menjant o no. npossibles s'inicialitza a 0.
 	//metode per afegir moviment a llista y mirar consecutius(?) // 
 	// Posició[MAX_POSICIONS] getPosicionsValides(const Posicio& origen int& nPosValides);
 	// afegeixMoviment(Moviment& nouMoviment);
