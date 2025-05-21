7220371d7f782bf0d02e0d72315c9dd6e19f6dbb
NIU1744801 <1744801@uab.cat>
2025-05-21 19:36:28 +0200

    Inicio de graficos
    Hecho casi todo el pwp de la libreria gràfica

---
+++ Source/0. C++ Code/Logic Game/joc.cpp
@@ -0,0 +1,84 @@
+//
+//  CurrentGame.cpp
+//  LearnChess
+//
+//  Created by Enric Vergara on 21/2/22.
+//
+
+#include "joc.hpp"
+#include <iostream>
+#include <fstream>
+#include "GraphicManager.h"
+
+Joc::Joc()
+{
+	GraphicManager::getInstance()->drawSprite(GRAFIC_FONS, 0, 0);
+	GraphicManager::getInstance()->drawSprite(GRAFIC_TAULER, POS_X_TAULER, POS_Y_TAULER);
+}
+
+bool Joc::actualitza(int mousePosX, int mousePosY, bool mouseStatus) 
+{
+	//TODO 1: Interactuar amb la crida per dibuixar gràfics (sprites).
+	// 	      Dibuixar a pantalla el fons i el gràfic amb el tauler buit.
+	//------------------------------------------------------------------
+	//TODO 1.1 Afegir l'include de GraphicManager --> #include "GraphicManager.h"
+	//TODO 1.2 Fer la crida de dibuixar un sprite --> GraphicManager::getInstance()->drawSprite(image, posX, posY);
+	//	    Per començar podem cridar el drawSprite amb els params --> 
+	//          (GRAFIC_FONS,0,0) i 
+	//          (GRAFIC_TAULER, POS_X_TAULER, POS_Y_TAULER)
+
+ 
+
+
+	//TODO 1.3: Dibuixar a pantalla el gràfic amb el tauler i una fitxa blanca a la posició (fila, columna ) del tauler
+
+	int fila = (mousePosY - POS_Y_TAULER)/ALCADA_CASELLA;
+	int columna = (mousePosX-POS_X_TAULER)/AMPLADA_CASELLA;
+	if ((mouseStatus) && (mousePosX >= (POS_X_TAULER + CASELLA_INICIAL_X)) &&
+		(mousePosY >= POS_Y_TAULER + CASELLA_INICIAL_Y) &&
+		(mousePosX <= (POS_X_TAULER + CASELLA_INICIAL_X + AMPLADA_CASELLA * NUM_COLS_TAULER)) &&
+		(mousePosY <= (POS_Y_TAULER + CASELLA_INICIAL_Y + ALCADA_CASELLA * NUM_FILES_TAULER)))
+	{
+		GraphicManager::getInstance()->drawSprite(GRAFIC_FONS, 0, 0);
+		GraphicManager::getInstance()->drawSprite(GRAFIC_TAULER, POS_X_TAULER, POS_Y_TAULER);
+		int posX = POS_X_TAULER + CASELLA_INICIAL_X + ((columna - 1) * AMPLADA_CASELLA);
+		int posY = POS_Y_TAULER + CASELLA_INICIAL_Y + ((fila - 1) * ALCADA_CASELLA);
+		GraphicManager::getInstance()->drawSprite(GRAFIC_FITXA_BLANCA, posX, posY);
+	}
+	//TODO 2: Interactuar amb el ratolí.
+	//------------------------------------------------------------------
+	//TODO 2.1: Dibuixar la fitxa blanca al tauler només si estem pressionant el botó del ratolí
+
+
+
+    //TODO 2.2: Dibuixar la fitxa blanca al tauler només si estem pressionant el botó del ratolí i el ratolí
+    // està dins del límits del tauler
+
+ 
+
+    //TODO 2.3: Dibuixar la fitxa blanca al tauler només si estem pressionant el botó del ratolí i el ratolí
+    // està dins del límits del tauler. Dibuixa la fitxa a la casella on està el ratolí
+
+
+
+    //TODO 2.4: Dibuixar la fitxa blanca a la casella on cliquem al ratolí. La fitxa s'ha de mantenir dibuixada
+    // a la casella quan deixem de clicar amb el ratolí. Quan cliquem a una altra casella, la fitxa canvia de posició
+    // a la nova casella
+
+        
+        
+    // TODO 3: Imprimir text per pantalla
+    //------------------------------------------
+    // TODO 3.1: Mostrar la posició actual del ratolí a sota del tauler
+
+	int posTextX = POS_X_TAULER;
+	int posTextY = POS_Y_TAULER + (ALCADA_CASELLA * NUM_FILES_TAULER) + 120;
+	std::string msg = "PosX: " + to_string(mousePosX) + ", PosY: " + to_string(mousePosY);
+	GraphicManager::getInstance()->drawFont(FONT_WHITE_30, posTextX, posTextY, 0.8, msg);
+
+	return false;
+}
+
+
+
+
